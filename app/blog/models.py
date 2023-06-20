7b726d6610410def43e5f277a85e504b3ef531fb
Emil <48238167+emka6451@users.noreply.github.com>
2023-06-20 21:43:45 +0400

    add gallery filter and fix mobile logo (add mobilefoto in model)

--- app/blog/models.py
+++ app/blog/models.py
@@ -7,9 +7,6 @@ from django_resized import ResizedImageField
 from django.utils.translation import gettext_lazy as _
 
 
-
-
-
 class IndexSlider(models.Model):
     little_title = models.CharField(max_length=500)
     big_title = models.CharField(max_length=500)
@@ -28,8 +25,8 @@ class WhoWeAre(models.Model):
 
     experience = models.IntegerField()
     image = models.FileField()
-    title1 = models.CharField(max_length=200,null=True)
-    title2 = models.CharField(max_length=200,null=True)
+    title1 = models.CharField(max_length=200, null=True)
+    title2 = models.CharField(max_length=200, null=True)
     # banner = models.FileField()
 
     class Meta:
@@ -39,7 +36,8 @@ class WhoWeAre(models.Model):
 
 class WhoTitle1(models.Model):
     name = models.CharField(max_length=200)
-    who = models.ForeignKey(WhoWeAre,on_delete=models.CASCADE, related_name="title_1")
+    who = models.ForeignKey(
+        WhoWeAre, on_delete=models.CASCADE, related_name="title_1")
 
     class Meta:
         verbose_name = "Title 1"
@@ -48,12 +46,12 @@ class WhoTitle1(models.Model):
 
 class WhoTitle2(models.Model):
     name = models.CharField(max_length=200)
-    who = models.ForeignKey(WhoWeAre,on_delete=models.CASCADE, related_name="title_2")
+    who = models.ForeignKey(
+        WhoWeAre, on_delete=models.CASCADE, related_name="title_2")
 
     class Meta:
         verbose_name = "Title 2"
         verbose_name_plural = "Title 2"
-
 
 
 class Service(models.Model):
@@ -78,19 +76,17 @@ class Service(models.Model):
         return reverse('service_detail', kwargs={"slug": self.slug})
 
 
-
 class ServiceOffers(models.Model):
     title = models.CharField(max_length=200)
-    service = models.ForeignKey(Service,on_delete=models.CASCADE, related_name="offers")
-
-
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="offers")
 
 
 class WhyUs(models.Model):
     title = models.CharField(max_length=300)
     # text = models.TextField()
     video_link = models.TextField(null=True)
-    video_image = models.ImageField(upload_to="VideoImage",null=True)
+    video_image = models.ImageField(upload_to="VideoImage", null=True)
     text = RichTextField()
 
     class Meta:
@@ -103,7 +99,8 @@ class WhyOffers(models.Model):
     # text = models.TextField()
     text = RichTextField()
 
-    why = models.ForeignKey(WhyUs,on_delete=models.CASCADE, related_name="offers")
+    why = models.ForeignKey(
+        WhyUs, on_delete=models.CASCADE, related_name="offers")
 
     def __str__(self):
         return self.title
@@ -111,8 +108,6 @@ class WhyOffers(models.Model):
     class Meta:
         verbose_name = "Offer"
         verbose_name_plural = "Offers"
-
-
 
 
 class Testimonial(models.Model):
@@ -125,24 +120,20 @@ class Testimonial(models.Model):
     def __str__(self):
         return self.author
 
-
     class Meta:
         verbose_name = "Testimonial"
         verbose_name_plural = "Testimonials"
 
 
-
 class Appointment(models.Model):
     name = models.CharField(max_length=300)
     email = models.EmailField()
-    date = models.CharField(max_length=300,null=True)
-    phone = models.CharField(max_length=20,null=True)
+    date = models.CharField(max_length=300, null=True)
+    phone = models.CharField(max_length=20, null=True)
     message = models.TextField(null=True)
-
 
     def __str__(self):
         return self.email
-
 
 
 class Gallery(models.Model):
@@ -154,11 +145,9 @@ class Gallery(models.Model):
         return self.link_name
 
 
-
-
 class ArticleCategory(models.Model):
-    name = models.CharField(max_length=300,unique=True)
-    slug = models.SlugField(editable=False, null=True,unique=True)
+    name = models.CharField(max_length=300, unique=True)
+    slug = models.SlugField(editable=False, null=True, unique=True)
 
     def __str__(self):
         return self.name
@@ -172,20 +161,19 @@ class ArticleCategory(models.Model):
         return reverse('category_detail', kwargs={"slug": self.slug})
 
 
-
 class Article(models.Model):
     # author = models.ForeignKey(Author,on_delete=models.CASCADE, related_name="article",null=True)
-    title = models.CharField(max_length=300,unique=True)
+    title = models.CharField(max_length=300, unique=True)
     image = models.FileField(upload_to="Article")
     context = RichTextField()
-    slug = models.SlugField(editable=False, null=True,unique=True)
-    click = models.PositiveIntegerField(default=0,editable=False)
-    category = models.ForeignKey(ArticleCategory,on_delete=models.CASCADE, related_name="articles",null=True)
+    slug = models.SlugField(editable=False, null=True, unique=True)
+    click = models.PositiveIntegerField(default=0, editable=False)
+    category = models.ForeignKey(
+        ArticleCategory, on_delete=models.CASCADE, related_name="articles", null=True)
     article_banner = models.ImageField(upload_to="article_banner", null=True)
     order = models.IntegerField(('sıra'), default=0, )
     # date = models.DateField(auto_now_add=True)
     date = models.DateField()
-
 
     def __str__(self):
         return self.title
@@ -201,27 +189,42 @@ class Article(models.Model):
 
 class GeneralSettings(models.Model):
 
-    site_title = models.CharField(max_length=1500, verbose_name="Saytın başlığı")
-    email = models.CharField(max_length=1500, verbose_name="Email", null=True,blank=True)
-    number_1 = models.CharField(max_length=1500, verbose_name="Nömrə", null=True,blank=True)
-    number_2 = models.CharField(max_length=1500, verbose_name="Nömrə", null=True,blank=True)
-    number_3 = models.CharField(max_length=1500, verbose_name="Nömrə", null=True,blank=True)
+    site_title = models.CharField(
+        max_length=1500, verbose_name="Saytın başlığı")
+    email = models.CharField(
+        max_length=1500, verbose_name="Email", null=True, blank=True)
+    number_1 = models.CharField(
+        max_length=1500, verbose_name="Nömrə", null=True, blank=True)
+    number_2 = models.CharField(
+        max_length=1500, verbose_name="Nömrə", null=True, blank=True)
+    number_3 = models.CharField(
+        max_length=1500, verbose_name="Nömrə", null=True, blank=True)
 
     adress = models.CharField(max_length=1500, verbose_name="Ünvan", null=True)
-    g_adress = models.CharField(max_length=1500, verbose_name="Google Map linki", null=True,blank=True)
-    logo = models.FileField(verbose_name="logo(138x35)", blank=True,upload_to="general_settings")
-    favicon = models.FileField(verbose_name="favicon(100x100)", blank=True, null=True,upload_to="general_settings")
+    g_adress = models.CharField(
+        max_length=1500, verbose_name="Google Map linki", null=True, blank=True)
+    logo = models.FileField(verbose_name="logo(138x35)",
+                            blank=True, upload_to="general_settings")
+    favicon = models.FileField(verbose_name="favicon(100x100)",
+                               blank=True, null=True, upload_to="general_settings")
 
-    footer_logo = models.FileField(verbose_name="Footer logo(150x38)",help_text="Saytın aşağısındakı logo", blank=True, null=True,upload_to="general_settings")
+    footer_logo = models.FileField(verbose_name="Footer logo(150x38)",
+                                   help_text="Saytın aşağısındakı logo", blank=True, null=True, upload_to="general_settings")
 
-    facebook = models.CharField(max_length=1500, verbose_name="Facebook", blank=True)
+    mobile_logo = models.FileField(verbose_name="Mobile logo(150x38)",
+                                   help_text="Mobile logo", blank=True, null=True, upload_to="general_settings")
+
+    facebook = models.CharField(
+        max_length=1500, verbose_name="Facebook", blank=True)
     # linkedin = models.CharField(max_length=1500, verbose_name="Linkedin", blank=True)
-    instagram = models.CharField(max_length=1500, verbose_name="Instagram", blank=True)
-    youtube = models.CharField(max_length=1500, verbose_name="Youtube", blank=True)
-    tiktok = models.CharField(max_length=1500, verbose_name="TikTok", blank=True)
+    instagram = models.CharField(
+        max_length=1500, verbose_name="Instagram", blank=True)
+    youtube = models.CharField(
+        max_length=1500, verbose_name="Youtube", blank=True)
+    tiktok = models.CharField(
+        max_length=1500, verbose_name="TikTok", blank=True)
 
-
-    footer_text = models.TextField(verbose_name="Footer Ucun tekst",null=True)
+    footer_text = models.TextField(verbose_name="Footer Ucun tekst", null=True)
 
     def __str__(self):
         return ('%s') % (self.site_title)
@@ -232,16 +235,13 @@ class GeneralSettings(models.Model):
         # ordering = ['-id']
 
 
-
 class AboutOffers(models.Model):
     image = models.ImageField(upload_to="AboutOffers")
     title = models.CharField(max_length=300)
     text = models.TextField()
 
-
     def __str__(self):
         return self.title
-
 
 
 class Contact(models.Model):
@@ -251,18 +251,10 @@ class Contact(models.Model):
     subject = models.CharField(max_length=100)
     message = models.TextField()
 
-
     def __str__(self):
         return self.email
 
 
-
 class Certificate(models.Model):
     # certificate_image = ResizedImageField(size=[410, 410], upload_to='certificate_images', blank=True, null=True)
-    certificate_image = models.ImageField(
-        _('image'),
-        upload_to='certificate_images',
-        null=True,
-        blank=True,
-        help_text=_('W: 410, H: 410')
-    )
+    certificate_image = models.FileField(upload_to="certificate_image")
