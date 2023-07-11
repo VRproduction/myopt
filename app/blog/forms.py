d1be7144320d8c2a7340ff0561c4a9c4ebdbaeb9
ilkinanvarlee <ilkine2191@gmail.com>
2023-07-11 17:47:56 +0400

    flag is ready

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -1,46 +1,38 @@
 from django import forms
 from .models import *
-
+from django.utils.translation import gettext_lazy as _
 
 
 class AppointmentForm(forms.ModelForm):
 
-    name = forms.CharField(max_length=1200, widget=forms.TextInput(attrs={
+    name = forms.CharField(max_length=1200, widget=forms.TextInput(attrs={'type': 'text',
+                                                                          'name': 'name',
+                                                                          'placeholder': 'Ad, Soyad'
+                                                                          }))
 
-        'type': 'text',
-        'name':'name',
-        'placeholder':'Ad, Soyad'
+    email = forms.EmailField(label=_('Email'), max_length=100, widget=forms.EmailInput(attrs={'type': 'email',
+                                                                                              'name': 'email',
+                                                                                              'placeholder': 'E-mail'}))
 
-    }))
-
-    email = forms.EmailField(max_length=100, widget=forms.EmailInput(attrs={
-
-
-
-        'type': 'email',
-        'name':'email',
-        'placeholder':'E-mail'
-
-
-    }
-    ))
-
-
-    date = forms.CharField(max_length=1200, widget=forms.TextInput(attrs={
-
-        'type': 'text',
-        'name': 'date',
-        'placeholder': 'Görüş tarixi',
-        'id': 'datepicker'
-    }))
+    date = forms.CharField(label=_('Görüş tarixi'), max_length=1200, widget=forms.TextInput(attrs={'type': 'text',
+                                                                                                   'name': 'date',
+                                                                                                   'placeholder': 'Görüş tarixi',
+                                                                                                   # 'id': 'datepicker'
+                                                                                                   }))
 
 
     class Meta:
         model = Appointment
         fields = '__all__'
+        localized_fields = '__all__'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['placeholder'] = 'Ad, Soyad'
+        self.fields['email'].widget.attrs['placeholder'] = 'E-mail'
+        self.fields['date'].widget.attrs['placeholder'] = 'Görüş tarixi'
+
         for key, field in self.fields.items():
             field.label = ""
 
