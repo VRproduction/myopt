bbbc69f2c665aa5ff797ebc294e0f2cfb871dd30
ilgarhasanzada <ilqarhsnzad@gmail.com>
2024-01-16 18:11:27 +0400

    add docker-compose file

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -92,13 +92,23 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myopt_dev',
+        'USER': 'myopt',
+        'PASSWORD': 'myopt',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
-
 
 
 # Password validation
